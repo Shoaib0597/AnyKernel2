919d50f9c9638d6bc5d60282237afddaa5e47d2b
Shoaib0597 <Shoaib0595@gmail.com>
2018-07-30 15:29:10 +0530

    AnyKernel2: Configure MiniVelocity Kernel for Yu Yureka Black (Garlic)
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,20 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=MiniVelocity Kernel by Shoaib Anwar (@Shoaib05)
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=garlic
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
